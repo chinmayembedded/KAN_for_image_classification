a2a2553ea15714a974f16154d7019a7ec9466e01
Noisecape <tommycaps@hotmail.it>
2024-05-26 23:08:51 +0000

    modify bug to load cifar100

--- kan_classifiers/data/data.py
+++ kan_classifiers/data/data.py
@@ -42,8 +42,8 @@ class DataPipeline:
 
         transforms_train = T.Compose([T.ToTensor()]) # add any extra transforms for training
         transforms_test = T.Compose([T.ToTensor()])
-        trainset = datasets.CIFAR10(root=self.configs['cifar100']['data_path'], train=True, download=True, transform=transforms_train)
-        testset = datasets.CIFAR10(root=self.configs['cifar100']['data_path'], train=False, download=True, transform=transforms_test)
+        trainset = datasets.CIFAR100(root=self.configs['cifar100']['data_path'], train=True, download=True, transform=transforms_train)
+        testset = datasets.CIFAR100(root=self.configs['cifar100']['data_path'], train=False, download=True, transform=transforms_test)
         train_load = DataLoader(trainset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
         testset_load = DataLoader(testset, batch_size=1, shuffle=False, num_workers=num_workers)
 
